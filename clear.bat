46a9b0ca0c45abfaf5549e0ae10c0f3d02e10b69
KJPOLWIN <85358420+KJPOLWIN@users.noreply.github.com>
2022-05-30 22:39:57 +0200

    Basic project structure

---
+++ clear.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rmdir CMakeFiles /s
+del cmake_install.cmake CMakeCache.txt screamer-learner.exe Makefile 
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(1920, 1080), "", sf::Style::Fullscreen);
+  window.setFramerateLimit(60);
+  window.setKeyRepeatEnabled(true);
+
+  sf::Event event{  };
+  
+  while(window.isOpen())
+  {
+    while(window.pollEvent(event))
+    {
+      if(event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+      else if(event.type == sf::Event::KeyPressed
+           && event.key.code == sf::Keyboard::Escape)
+      {
+        window.close();
+      }
+    }
+
+    window.clear();
+
+
+
+    window.display();
+  }
+
+  return 0;
+}
